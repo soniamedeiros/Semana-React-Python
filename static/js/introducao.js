eea9ae4888d209ae4473fced893dc6e0eb2a0886
soniamedeiros <soniamagosso@gmail.com>
2022-10-16 06:08:00 -0300

    Frontend e Backend

---
+++ static/js/introducao.js
@@ -0,0 +1,11 @@
+// comando qu exibe informações no console
+// console.log("Olá Mundo!")
+
+// let nome = "Sonia Barros"
+// console.log(nome)
+
+// let idade = 26
+// console.log(idade)
+
+// let ehProgramador = true
+// console.log(ehProgramador)
